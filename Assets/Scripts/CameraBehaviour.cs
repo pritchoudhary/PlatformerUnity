15d55af754cd262a1bf1504b246226b143748c80
Pritish Choudhary <pritish.choudhary@yahoo.in>
2021-04-08 17:07:59 +1000

    Base SetUp
    Created a base level for the game. This consists of the camera and the playerObject. Set the camera up to be a top down camera and added a script that would follow the player smoothly at all times

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Top down camera that would follow the player with an offset at all times
+public class CameraBehaviour : MonoBehaviour
+{
+    //Variables
+    public Transform playerTarget;
+    public Vector3 targetOffset;
+    public float moveSpeed = 1.5f;
+
+    private Transform cameraTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraTransform = transform;
+    }
+    
+    //Using fixed update to smoothen the camera movement and hence using fixedDeltaTime instead of deltaTime
+    void FixedUpdate()
+    {
+        //Check if there's are target to follow and then transform camera position to the player
+        if(playerTarget != null)
+        {
+            cameraTransform.position = Vector3.Lerp(cameraTransform.position, playerTarget.position + targetOffset, moveSpeed*Time.fixedDeltaTime);
+        }
+        else
+        {
+            Debug.LogError("Attach Player object to the camera");
+        }
+    }
+
+    public void SetPlayerTarget(Transform playerTransform)
+    {
+        playerTarget = playerTransform;
+    }
+}
